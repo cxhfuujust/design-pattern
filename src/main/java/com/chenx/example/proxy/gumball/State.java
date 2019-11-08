9a965ee02d133d1100edc4e6926de414251d48b9
cxhfuujust <cxhfuujust@yeah.net>
2019-11-08 16:49:01 +0800

    design pattern commit!

---
+++ src/main/java/com/chenx/example/proxy/gumball/State.java
@@ -0,0 +1,13 @@
+package com.chenx.example.proxy.gumball;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+	public void insertQuarter();
+
+	public void ejectQuarter();
+
+	public void turnCrank();
+
+	public void dispense();
+}
